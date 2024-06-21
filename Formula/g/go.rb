af1b85aa3827bb76c07f71ac2eded0dcac8e1e92
Štefan Baebler <stefan.baebler@gmail.com>
2024-06-21 19:25:16 +0200

    go 1.23rc1
    https://groups.google.com/g/golang-announce/c/8ciOP5ve_CM

--- Formula/g/go.rb
+++ Formula/g/go.rb
@@ -1,9 +1,9 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.22.4.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.22.4.src.tar.gz"
-  sha256 "fed720678e728a7ca30ba8d1ded1caafe27d16028fab0232b8ba8e22008fb784"
+  url "https://go.dev/dl/go1.23rc1.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.23rc1.src.tar.gz"
+  sha256 "6e9c4765872808663ccf0b937e5637c7df67146943fa6e8909a81a0d129c8045"
   license "BSD-3-Clause"
   head "https://go.googlesource.com/go.git", branch: "master"
 
